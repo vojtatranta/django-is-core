4f025cb4a8651e45e14673ee615ddc795f373048
vojtatranta <vojta.tranta@gmail.com>
2015-04-06 17:52:35 +0200

    Fixed dependecies for heroku

--- setup.py
+++ setup.py
@@ -30,22 +30,16 @@ setup(
     install_requires=[
         'django>=1.6',
         'django-class-based-auth-views>=0.2',
-        'django-piston==1.1.13',
-        'germanium>=0.2.4',
-        'django-block-snippets==0.0.11',
-        'django-chamber>=0.0.9',
         'python-dateutil>=2.2',
         'pytz',
         'django-apptemplates',
         'Unidecode>=0.04.16',
         'factory-boy>=2.3.1',
-        'django-project-info==0.2.4',
         'mimeparse==0.1.3',
         'sorl-thumbnail==11.12',
         'pillow==2.3.0'
     ],
     dependency_links=[
-        'https://github.com/matllubos/django-piston/tarball/1.1.13#egg=django-piston-1.1.13',
         'https://github.com/matllubos/django-chamber/tarball/0.0.9#egg=django-chamber-0.0.9',
         'https://github.com/matllubos/django-block-snippets/tarball/0.0.11#egg=django-block-snippets-0.0.11',
         'https://github.com/LukasRychtecky/germanium/tarball/0.2.4#egg=germanium-0.2.4',
